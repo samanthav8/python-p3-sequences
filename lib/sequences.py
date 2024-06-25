75c690e9c9265eeef37a2bd4a0209f9a82f68870
Samantha <samanthavillafana8@gmail.com>
2024-06-25 19:11:08 -0400

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+        return
+    
+    fibonacci_sequence = [0, 1]
+    while len(fibonacci_sequence) < length:
+        nextnum = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+        fibonacci_sequence.append(nextnum)
+    
+    print(fibonacci_sequence[:length])
